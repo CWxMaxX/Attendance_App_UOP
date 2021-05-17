93855194e605aa6a8b839c6e9a5fdaa5deb9868d
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-05-17 12:30:42 +0530

    Update Login Page

--- App.js
+++ App.js
@@ -12,34 +12,7 @@ import {
 import backgroundImage from "./assets/Images/loginPage.png";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={backgroundImage}
-        style={styles.backgroundImageStyle}
-      >
-        <View style={styles.loginCard}>
-          <Text
-            style={
-              (styles.cardText,
-              { fontSize: 50, marginBottom: 70, color: "#000" })
-            }
-          >
-            Sign In
-          </Text>
-          <TextInput style={styles.inputStyle} placeholder="Username" />
-          <TextInput style={styles.inputStyle} placeholder="Password" />
-          <Text style={{ fontSize: 12, color: "#fff", marginTop: 10 }}>
-            Forgot Password
-          </Text>
-          <TouchableOpacity style={styles.button}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>Sign In</Text>
-          </TouchableOpacity>
-          <Text style={styles.cardText}></Text>
-        </View>
-      </ImageBackground>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
@@ -48,45 +21,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#FFF259",
-  },
-  loginCard: {
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "#FEF7A7",
-    width: "80%",
-    height: 550,
-    marginTop: 90,
-  },
-  backgroundImageStyle: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    marginTop: 60,
-  },
-  cardText: {
-    margin: 10,
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 100,
-  },
-  inputStyle: {
-    width: "80%",
-    color: "#fff",
-    textAlign: "center",
-    borderWidth: 2,
-    borderColor: "#fff",
-    borderRadius: 20,
-    marginTop: 15,
-    fontSize: 18,
-    height: 45,
-  },
-  button: {
-    width: "50%",
-    backgroundColor: "#FFF259",
-    alignItems: "center",
-    marginTop: 15,
-    padding: 10,
-    borderRadius: 20,
   },
 });
