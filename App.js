c3f91da7679b375eae323e70edc74d54e55f7570
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-05-17 12:22:43 +0530

    Updatte Login Page

--- App.js
+++ App.js
@@ -1,50 +1,92 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-// import * as firebase from "firebase";
-// var firebaseConfig = {
-//   apiKey: "AIzaSyAtWWg95VPkrUuH7FSzlirildOd4QI5p4Q",
-//   authDomain: "attendanceapp-9be34.firebaseapp.com",
-//   projectId: "attendanceapp-9be34",
-//   storageBucket: "attendanceapp-9be34.appspot.com",
-//   messagingSenderId: "932726945174",
-//   appId: "1:932726945174:web:0aa7c57cbaf6fb32ac59b7",
-//   measurementId: "G-2T1VSM5DSH",
-// };
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import backgroundImage from "./assets/Images/loginPage.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Attendance App CWx</Text>
-      <TextInput
-        placeholder="Username"
-        style={{
-          borderBottomColor: "#DA8E09",
-          borderBottomWidth: 2,
-          marginTop: 150,
-          marginBottom: 15,
-        }}
-      />
-      <TextInput
-        placeholder="Password"
-        style={{
-          borderBottomColor: "#DA8E09",
-          borderBottomWidth: 2,
-          marginBottom: 15,
-        }}
-      />
-      <Button title="Sign in" color="#DA8E09" />
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.backgroundImageStyle}
+      >
+        <View style={styles.loginCard}>
+          <Text
+            style={
+              (styles.cardText,
+              { fontSize: 50, marginBottom: 70, color: "#000" })
+            }
+          >
+            Sign In
+          </Text>
+          <TextInput style={styles.inputStyle} placeholder="Username" />
+          <TextInput style={styles.inputStyle} placeholder="Password" />
+          <Text style={{ fontSize: 12, color: "#fff", marginTop: 10 }}>
+            Forgot Password
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>Sign In</Text>
+          </TouchableOpacity>
+          <Text style={styles.cardText}></Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
     flex: 1,
-    backgroundColor: "#fff259",
-    // alignItems: "center",
-    // justifyContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF259",
+  },
+  loginCard: {
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "#FEF7A7",
+    width: "80%",
+    height: 550,
+    marginTop: 90,
+  },
+  backgroundImageStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginTop: 60,
+  },
+  cardText: {
+    margin: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 100,
+  },
+  inputStyle: {
+    width: "80%",
+    color: "#fff",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#fff",
+    borderRadius: 20,
+    marginTop: 15,
+    fontSize: 18,
+    height: 45,
+  },
+  button: {
+    width: "50%",
+    backgroundColor: "#FFF259",
+    alignItems: "center",
+    marginTop: 15,
+    padding: 10,
+    borderRadius: 20,
   },
 });
